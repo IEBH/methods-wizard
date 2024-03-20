f63da4cdd772016f649952d421a87432bab13b05
Octian <115122605+Octian@users.noreply.github.com>
2024-03-20 15:11:47 +1000

    title page

--- src/project-templates/methods-wizard/ViewTitlePagePeople.vue
+++ src/project-templates/methods-wizard/ViewTitlePagePeople.vue
@@ -1,32 +1,29 @@
 <template>
-	<div>
-		<h1>People</h1>
+  <div>
+    <h1>People</h1>
 
-		<InputTextSingleLineMulti
-			question="Who are the authors in the study?"
-			:value="titlepage.authors"
-			@input="updateField('authors', $event)"
-			placeholder="e.g. Justin Clark"
-		/>
+    <InputTextSingleLineMulti
+      question="Who are the authors in the study?"
+      :value="$tera.state.author"
+      placeholder="e.g. Justin Clark"
+    />
 
-		<InputTextSingleLineMulti
-			question="Who else helped with the study? (Acknowledgements)"
-			:value="titlepage.acknowledgements"
-			@input="updateField('acknowledgements', $event)"
-			placeholder="e.g. Justin Clark"
-		/>
-	</div>
+    <InputTextSingleLineMulti
+      question="Who else helped with the study? (Acknowledgements)"
+      :value="$tera.state.acknowledgements"
+      placeholder="e.g. Justin Clark"
+    />
+  </div>
 </template>
 <script>
 import InputTextSingleLineMulti from "@/components/InputTextSingleLineMulti.vue";
 
-import deepstreamMixin from "@/mixins/DeepstreamMixin";
 export default {
-	name: "ViewTitlePagePeople",
-	mixins: [deepstreamMixin("titlepage")],
-	components: {
-		InputTextSingleLineMulti
-	}
+  name: "ViewTitlePagePeople",
+
+  components: {
+    InputTextSingleLineMulti
+  }
 };
 </script>
 <!-- Add "scoped" attribute to limit CSS to this component only -->
--- src/project-templates/methods-wizard/ViewTitlepage.vue
+++ src/project-templates/methods-wizard/ViewTitlepage.vue
@@ -1,24 +1,24 @@
 <template>
-	<div>
-		<h1>Title Page</h1>
-		<InputTextSingleLine
-			v-model="$tera.state.name"
-			question="What is the title of the study?"
-			placeholder="e.g. Blue-light therapy for acne vulgaris: a systematic review and meta-analysis"
-		/>
-		<InputTextSingleLine
-			v-model="$tera.state.nameShort"
-			question="What is the short title for the study?"
-			placeholder="e.g. Blue-light"
-		/>
+  <div>
+    <h1>Title Page</h1>
+    <InputTextSingleLine
+      v-model="$tera.state.title"
+      question="What is the title of the study?"
+      placeholder="e.g. Blue-light therapy for acne vulgaris: a systematic review and meta-analysis"
+    />
+    <InputTextSingleLine
+      v-model="$tera.state.titleshort"
+      question="What is the short title for the study?"
+      placeholder="e.g. Blue-light"
+    />
 
-		<InputTextNumber
-			v-model="$tera.state.year"
-			question="What year will study be published?"
-		/>
+    <InputTextNumber
+      v-model="$tera.state.year"
+      question="What year will study be published?"
+    />
 
-		<pre>$TERA.state: {{$tera.state}}</pre>
-	</div>
+    <pre>$TERA.state: {{ $tera.state }}</pre>
+  </div>
 </template>
 
 <script>
@@ -26,10 +26,10 @@ import InputTextNumber from "@/components/InputTextNumber.vue";
 import InputTextSingleLine from "@/components/InputTextSingleLine.vue";
 
 export default {
-	name: "ViewMethodPicot",
-	components: {
-		InputTextSingleLine,
-		InputTextNumber
-	},
+  name: "ViewMethodPicot",
+  components: {
+    InputTextSingleLine,
+    InputTextNumber
+  }
 };
 </script>
